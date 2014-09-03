c39ad863202f146d633b992078d6ddb426d5acf1
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-09-03 18:18:08 -0400

    added solutions

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,22 @@
 #code your answers here
+
+
+def check_it_out
+	'Check out my sweet new ride, dude!'
+end
+
+def no_big_deal(car)
+	"No big deal, it's a #{car}"
+end
+
+def stolen
+	"dude, where's my car?"
+end
+
+def reporting_theft(car)
+	"OFFICER, SOMEONE'S STOLEN MY #{car.upcase}"
+end
+
+def how_long_until_i_can_get_another_car(price, wage)
+	price/wage
+end
