fbe3519e93847ccde5ec21669cf1ac06266dac53
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-09-03 18:11:44 -0400

    tests written

---
+++ spec/sweet_ride_spec.rb
@@ -0,0 +1,35 @@
+require_relative "./spec_helper"
+
+describe "SUPER AWESOME CAR" do
+	
+	describe "#check_it_out" do
+		it "returns 'Check out my sweet new ride, dude!'" do
+			expect(check_it_out).to eq('Check out my sweet new ride, dude!')
+		end
+	end
+
+	describe "no_big_deal" do
+		it "returns a string 'no big deal, it's a ' with the argument added to it" do
+			expect(no_big_deal("Ferrari")).to eq("No big deal, it's a Ferrari")
+		end
+	end
+
+	describe "stolen" do
+		it "returns the string 'dude, where's my car?'" do
+			expect(stolen).to eq("dude, where's my car?")
+		end
+	end
+
+	describe "reporting_theft" do
+		it "returns 'OFFICER, SOMEONE'S STOLEN MY ' with the input argument, uppercased" do
+			expect(reporting_theft("buick")).to eq("OFFICER, SOMEONE'S STOLEN MY BUICK")
+		end
+	end
+
+	describe "how_long_until_i_can_get_another_car" do
+		it "returns hours you need to work based on input of car price and hourly wage" do
+			expect(how_long_until_i_can_get_another_car(7000, 10)).to eq(700)
+		end
+	end
+
+end
