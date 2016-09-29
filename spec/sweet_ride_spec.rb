cbcd026683c4f2c8129388ffec7b5913cb4eb9cb
Mr. Mueller <bmueller@hstat.org>
2016-09-29 23:13:03 +0000

    add extra test cases

--- spec/sweet_ride_spec.rb
+++ spec/sweet_ride_spec.rb
@@ -11,6 +11,7 @@ describe "SUPER AWESOME CAR" do
 	describe "no_big_deal" do
 		it "returns a string 'No big deal, it's a ' with the argument added to it" do
 			expect(no_big_deal("Ferrari")).to eq("No big deal, it's a Ferrari")
+			expect(no_big_deal("Lamborghini")).to eq("No big deal, it's a Lamborghini")
 		end
 	end
 
@@ -23,12 +24,14 @@ describe "SUPER AWESOME CAR" do
 	describe "reporting_theft" do
 		it "returns 'OFFICER, SOMEONE'S STOLEN MY ' with the input argument, uppercased" do
 			expect(reporting_theft("buick")).to eq("OFFICER, SOMEONE'S STOLEN MY BUICK")
+			expect(reporting_theft("toyota")).to eq("OFFICER, SOMEONE'S STOLEN MY TOYOTA")
 		end
 	end
 
 	describe "how_long_until_i_can_get_another_car" do
 		it "returns hours you need to work based on input of car price and hourly wage" do
 			expect(how_long_until_i_can_get_another_car(7000, 10)).to eq(700)
+			expect(how_long_until_i_can_get_another_car(6000, 12)).to eq(500)
 		end
 	end
 
