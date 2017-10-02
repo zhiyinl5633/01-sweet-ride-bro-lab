019cc836315bd7d16317bdc61d933b3ccee2ce2c
Kelly Lai <zhiyinl5633@hstat.org>
2017-10-02 15:49:57 +0000

    resolved failures

--- sweet_ride.rb
+++ sweet_ride.rb
@@ -1,1 +1,20 @@
-#code your answers here
+    #code your answers here
+def check_it_out
+     "Check out my sweet new ride, dude!"
+end
+
+def no_big_deal (car_name)
+    "No big deal, it's a #{car_name}"
+end
+
+def stolen
+    "dude, where's my car?"
+end
+
+def reporting_theft(car)
+    "OFFICER, SOMEONE'S STOLEN MY #{car.upcase}"
+end
+
+def how_long_until_i_can_get_another_car(car_price, hourly_wage)
+    car_price/hourly_wage
+end
